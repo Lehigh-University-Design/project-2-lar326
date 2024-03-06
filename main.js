f39cbceb7cbf88c449010f622b62c1d6b921b981
Kevin Lahoda <klahoda@gmail.com>
2024-03-06 09:14:04 -0500

    loop through videos

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const video = document.querySelector(".video-container video");
-const hoverText = document.querySelector(".video-container .hover-text");
 
-video.addEventListener("mouseenter", () => {
+const items = document.querySelectorAll('.video-container');
+
+items.forEach(item => {
+  // see change below
+  const video = item.querySelector('video');
+  const hoverText = item.querySelector('.hover-text');
+
+  video.addEventListener("mouseenter", () => {
     video.play();
+    console.log('okok');
     hoverText.classList.remove("active");
 });
 
 video.addEventListener("mouseleave", () => {
     video.pause();
     hoverText.classList.add("active");
+});
+  
 });
