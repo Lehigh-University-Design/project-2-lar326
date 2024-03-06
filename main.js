5c893aa97f971a9c536b041e226cbd2c919c93c7
lar326 <123413957+lar326@users.noreply.github.com>
2024-03-06 14:59:12 -0500

    Updates 1

--- main.js
+++ main.js
@@ -5,15 +5,14 @@ items.forEach(item => {
   const video = item.querySelector('video');
   const hoverText = item.querySelector('.hover-text');
 
-  video.addEventListener("mouseenter", () => {
-    video.play();
-    console.log('okok');
-    hoverText.classList.remove("active");
-});
-
-video.addEventListener("mouseleave", () => {
-    video.pause();
-    hoverText.classList.add("active");
-});
-  
+  video.addEventListener("click", () => {
+    if(video.paused){
+      video.play()
+      console.log('okok');
+      hoverText.classList.remove("active");
+    } else{
+      video.pause()
+      hoverText.classList.add("active");
+    }
+  });
 });
